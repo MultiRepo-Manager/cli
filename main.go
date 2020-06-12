bb8f14ae14e823905d48269a5ea7bf2dc464b358
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-06-12 22:39:23 +0200

    go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package main
 
-import "github.com/arrase/multi-repo-workspace/cli/cmd"
+import "github.com/arrase/multi-repo-workspace/cli"
 
 func main() {
 	cmd.Execute()
