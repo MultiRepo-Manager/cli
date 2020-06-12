b0f7d1956cf21cc2e3f36c90ae670fa6832b03f0
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-06-13 00:46:48 +0200

    go

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# Clean previous
+rm workspace-*
+
 # Build
-[ -d ../bin ] || mkdir ../bin
-env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -v -o ../bin/workspace-linux-amd64 .
-env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -v -o ../bin/workspace-darwin-amd64 .
+env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -v -o workspace-linux-amd64 .
+env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -v -o workspace-darwin-amd64 .
 
 # Compress
-cd ../bin
-cp workspace-linux-amd64 workspace-linux-amd64-upx
-cp workspace-darwin-amd64 workspace-darwin-amd64-upx
+upx -9 workspace-linux-amd64 -o workspace-linux-amd64-upx
+upx -9 workspace-darwin-amd64 -o workspace-darwin-amd64-upx
 
-upx -9 workspace-linux-amd64-upx
-upx -9 workspace-darwin-amd64-upx
+# Install
+cp workspace-$(go env GOOS)-$(go env GOARCH)-upx $GOBIN/workspace
