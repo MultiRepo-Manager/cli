d18e65940e2b1831fac3d1ca7d2b4e92199569e1
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-06-13 00:26:32 +0200

    go

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -v -o workspace-linux-amd64 . && upx -9 workspace-linux-amd64
-env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -v -o workspace-darwin-amd64 . && upx -9 workspace-darwin-amd64
+# Build
+[ -d ../bin ] || mkdir ../bin
+env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -v -o ../bin/workspace-linux-amd64 .
+env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -v -o ../bin/workspace-darwin-amd64 .
+
+# Compress
+cd ../bin
+cp workspace-linux-amd64 workspace-linux-amd64-upx
+cp workspace-darwin-amd64 workspace-darwin-amd64-upx
+
+upx -9 workspace-linux-amd64-upx
+upx -9 workspace-darwin-amd64-upx
