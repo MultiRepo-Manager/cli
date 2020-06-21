149c2a8658e322720a94bfc8d1f5dddc2fcf2b4d
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-06-21 12:47:02 +0200

    public files

--- build.sh
+++ build.sh
@@ -16,5 +16,8 @@ env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -v -o workspace-darwin-am
 upx -9 workspace-linux-amd64 -o workspace-linux-amd64-upx
 upx -9 workspace-darwin-amd64 -o workspace-darwin-amd64-upx
 
+# keep an eye on size
+ls -lh workspace-*
+
 # Install
 cp workspace-$(go env GOOS)-$(go env GOARCH)-upx $GOBIN/workspace
